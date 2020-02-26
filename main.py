222e0350f65f9ede18906755241b7b981e90213c
Sawyer Watts <sawyer.watts@gmail.com>
2020-02-25 22:05:17 -0800

    Add basic CLI option menu.

---
+++ main.py
@@ -0,0 +1,62 @@
+
+###############################################################################
+# "Public" Functions
+
+def cli():
+    shouldExit = False
+
+    while not shouldExit:
+        print()
+        print("This is the StopSpot data pipeline. Please select what you would like to do:")
+        print()
+        print("0 (or ctrl-d): Exit.")
+        print()
+
+        option = None
+        try:
+            option = _get_int(0, 0)
+        except EOFError:
+            option = 0
+
+        shouldExit = _handle_switch_case(option)
+
+
+###############################################################################
+# Private Functions
+
+def _get_int(min_value, max_value, cli_symbol="> "):
+    shouldContinue = True
+    while shouldContinue:
+        try:
+            option = input(cli_symbol)
+            option = int(option)
+
+            if option < min_value or option > max_value:
+                print("{" + str(option) + "} is not within range [" + str(min_value) + ", " + str(max_value) + "]; try again.")
+            else:
+                shouldContinue = False
+
+        except ValueError:
+            print("Please enter an integer.")
+
+    return option
+
+###########################################################
+
+def _handle_switch_case(option):
+    if option == 0:
+        print()
+        return True
+
+    else:
+        raise RuntimeError("A case that should not be reached has been reached.")
+
+    return False
+
+
+###############################################################################
+# Main
+
+if __name__ == "__main__":
+    cli()
+
