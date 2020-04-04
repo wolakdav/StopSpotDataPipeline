3784eea78e6d397937036f76d3d89ff568134f5b
Sawyer Watts <sawyer.watts@gmail.com>
2020-04-04 14:11:10 -0700

    Add tests for Service_Periods._prompt() and _print().

--- test/tables/test_service_periods.py
+++ test/tables/test_service_periods.py
@@ -1,3 +1,4 @@
+import io
 import pytest
 from sqlalchemy import create_engine
 from src.tables import Service_Periods
@@ -15,6 +16,7 @@ def dummy_engine():
     engine_info = "".join(["postgresql://", user, ":", passwd, "@", hostname, "/", db_name])
     return create_engine(engine_info), user, passwd, hostname, db_name
 
+
 def test_constructor_build_engine(dummy_engine):
     expected, user, passwd, hostname, db_name = dummy_engine
     instance = Service_Periods(user, passwd, hostname, db_name)
@@ -26,9 +28,13 @@ def test_constructor_given_engine(dummy_engine):
     instance = Service_Periods(engine=engine_url)
     assert instance._engine.url == engine.url
 
+def test_verbose(service_periods_fixture):
+    verbose = service_periods_fixture.verbose
+    assert isinstance(verbose, bool) and verbose == False
+
 def test_chunksize(service_periods_fixture):
     chunksize = service_periods_fixture._chunksize
-    assert int(chunksize) and chunksize > 1
+    assert isinstance(chunksize, int) and chunksize > 1
 
 def test_index_col(service_periods_fixture):
     assert service_periods_fixture._index_col == "service_key"
@@ -59,14 +65,53 @@ def test_creation_sql(service_periods_fixture):
 def test_get_engine(service_periods_fixture):
     assert service_periods_fixture.get_engine().url == service_periods_fixture._engine.url
 
-# TODO: mock out
+def test_print_unverbose(capsys, service_periods_fixture):
+    service_periods_fixture.verbose = False
+    service_periods_fixture._print("Hello!")
+    assert capsys.readouterr().out == ""
+
+def test_print_no_obj(capsys, service_periods_fixture):
+    service_periods_fixture.verbose = True
+    service_periods_fixture._print("Hello!")
+    assert capsys.readouterr().out == "Hello!\n"
+
+def test_print_no_obj_forced(capsys, service_periods_fixture):
+    service_periods_fixture.verbose = False
+    service_periods_fixture._print("Hello!", force=True)
+    assert capsys.readouterr().out == "Hello!\n"
+
+def test_print_obj(capsys, service_periods_fixture):
+    service_periods_fixture.verbose = True
+    obj = ["Pizza", "Pie"]
+    service_periods_fixture._print("Hello!", obj)
+    assert capsys.readouterr().out == "Hello!['Pizza', 'Pie']\n"
+
+def test_print_obj_forced(capsys, service_periods_fixture):
+    service_periods_fixture.verbose = False
+    obj = ["Pizza", "Pie"]
+    service_periods_fixture._print("Hello!", obj, True)
+    assert capsys.readouterr().out == "Hello!['Pizza', 'Pie']\n"
+
+def test_prompt_unhidden(capsys, monkeypatch, service_periods_fixture):
+    expected = "sw23"
+    prompt = "> "
+    monkeypatch.setattr("sys.stdin", io.StringIO(expected + "\n"))
+    result = service_periods_fixture._prompt(prompt)
+    assert result == expected
+
+def test_prompt_hidden(capsys, monkeypatch, service_periods_fixture):
+    expected = "fake\n"
+    prompt = "> "
+    monkeypatch.setattr("getpass.getpass", lambda _: expected)
+    result = service_periods_fixture._prompt(prompt, True)
+    assert result == expected
+
+# TODO: mock out DB and test:
 #   get_full_table
 #   create_schema
 #   delete_schema
 #   create_table
 #   delete_table
-
-# TODO: test prompt w/ hidden=True and False
 
 
 # TODO: When done testing this class, copy and adjust these tests for the other
