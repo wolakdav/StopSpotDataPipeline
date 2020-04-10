45411a612de0989dd8c36091e74481bf9d11ee5b
Eliot Kent Woodrich <ekwoodrich@gmail.com>
2020-04-09 17:36:46 -0700

    Added basic configurator class.

---
+++ src/config/Config.py
@@ -0,0 +1,30 @@
+
+import json
+from enum import Enum
+
+CONFIG_FILE = '/home/ekw/GitHub/TeamBeeCapstoneProject/src/config/config.json'
+
+
+class BoundsResult(Enum):
+    VALID = 1     #value is within range
+    MAX_ERROR = 2 #value is greater than MAX
+    MIN_ERROR = 3 #value is less than MIN
+
+class Config:    
+    def load(self, filename='log.txt', debug=False):
+        with open(CONFIG_FILE) as f:
+            self._data = json.load(f)
+            print(self._data)
+    def check_bounds(self, column_name, val):
+        if column_name in self._data['columns']:
+            col = self._data['columns'][column_name]
+
+            if col['max'] != 'NA':
+                if val > int(col['max']):
+                    return BoundsResult.MAX_ERROR
+
+            if col['min'] != 'NA':
+                if val < int(col['min']):
+                    return BoundsResult.MIN_ERROR
+
+            return BoundsResult.VALID
