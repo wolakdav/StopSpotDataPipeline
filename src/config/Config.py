a3b7dacba5fe968058de5521b2db0e106fb5f608
Eliot Kent Woodrich <ekwoodrich@gmail.com>
2020-04-09 17:51:59 -0700

    Added get_value() method to access arbitrary presets.

--- src/config/Config.py
+++ src/config/Config.py
@@ -14,6 +14,11 @@ class Config:
     def load(self, filename='log.txt', debug=False):
         with open(CONFIG_FILE) as f:
             self._data = json.load(f)
+
+    def get_value(self, val):
+        if val in self._data:
+            return self._data[val]
+            
     def check_bounds(self, column_name, val):
         if column_name in self._data['columns']:
             col = self._data['columns'][column_name]
