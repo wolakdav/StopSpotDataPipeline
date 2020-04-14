3511475dbd25ae8a3bbdde153005cd45f3b3b3d8
Eliot Kent Woodrich <ekwoodrich@gmail.com>
2020-04-14 15:23:17 -0700

    Restored cmd line handler.

--- src/config/Config.py
+++ src/config/Config.py
@@ -72,3 +72,10 @@ class Config:
                     return BoundsResult.MIN_ERROR
 
             return BoundsResult.VALID
+
+if __name__ == "__main__":
+     if len(sys.argv) > 1:
+         if (sys.argv[1] == "load"):
+             config = Config()
+             config.load()
+             print("Loaded config data.")
