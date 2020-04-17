d22dc70ed797e7744952fd1019548c141943c35e
David <awesomesauce712@gmail.com>
2020-04-16 19:08:55 -0700

    Fixed some PR stuff

--- test/interface/test_cli_with_args.py
+++ test/interface/test_cli_with_args.py
@@ -13,51 +13,46 @@ from src.tables import CTran_Data
 #     ai = ArgInterface()
 #     df = ai.query_with_args(ctran, ['--date-start=2019-03-01', '--date-end=2019-03-01'])
 #     assert len(df.index) == 71084
+@pytest.fixture
+def ai():
+    return ArgInterface()
 
-def test_no_cl_args_fails():
-    ai = ArgInterface()
+def test_no_cl_args_fails(ai):
     with pytest.raises(SystemExit) as sys_ext:
         ai._parse_cl_args(None)
     assert sys_ext.value.code == 2
 
-def test_one_garbage_cl_arg_fails():
-    ai = ArgInterface()
+def test_one_garbage_cl_arg_fails(ai):
     with pytest.raises(SystemExit) as sys_ext:
         ai._parse_cl_args(['t'])
     assert sys_ext.value.code == 2
 
-def test_two_garbage_cl_args_fails():
-    ai = ArgInterface()
+def test_two_garbage_cl_args_fails(ai):
     with pytest.raises(SystemExit) as sys_ext:
         ai._parse_cl_args(['t', 'u'])
     assert sys_ext.value.code == 2
 
-def test_one_empty_cl_arg_fails():
-    ai = ArgInterface()
+def test_one_empty_cl_arg_fails(ai):
     with pytest.raises(SystemExit) as sys_ext:
         ai._parse_cl_args(['--date-start='])
     assert sys_ext.value.code == 2
 
-def test_one_improper_cl_arg_fails():
-    ai = ArgInterface()
+def test_one_improper_cl_arg_fails(ai):
     with pytest.raises(SystemExit) as sys_ext:
         ai._parse_cl_args(['--date-start=202'])
     assert sys_ext.value.code == 2
 
-def test_one_proper_cl_arg_fails():
-    ai = ArgInterface()
+def test_one_proper_cl_arg_fails(ai):
     with pytest.raises(SystemExit) as sys_ext:
         ai._parse_cl_args(['--date-start=2020-01-01'])
     assert sys_ext.value.code == 2
 
-def test_one_proper_one_improper_cl_arg_fails():
-    ai = ArgInterface()
+def test_one_proper_one_improper_cl_arg_fails(ai):
     with pytest.raises(SystemExit) as sys_ext:
         ai._parse_cl_args(['--date-start=2020-01-01', '--date-end=2020-01-'])
     assert sys_ext.value.code == 2
 
-def test_two_proper_cl_args_succeeds():
-    ai = ArgInterface()
+def test_two_proper_cl_args_succeeds(ai):
     ai._parse_cl_args(['--date-start=2020-01-01', '--date-end=2020-01-02'])
 
 
