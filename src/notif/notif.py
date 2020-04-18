942335d3b30ce36c94010d5d467c03bd7be72a2d
Sawyer Watts <sawyer.watts@gmail.com>
2020-04-18 12:24:44 -0700

    Build basic _Notif.email().

--- src/notif/notif.py
+++ src/notif/notif.py
@@ -7,7 +7,7 @@ class _Notif(IOs):
 
     def __init__(self, config):
         super().__init__(True)
-        self.msg = "A critial error has occured."
+        self.msg = "A critical error has occured."
         self._port = 465  # For SSL
         self._config = config
 
@@ -19,25 +19,34 @@ class _Notif(IOs):
 
     #######################################################
 
-    def email(self, msg=""):
-        password = self._update_email_data()
+    # TODO: allow msg to be a list of paragrpahs?
+    def email(self, msg="", subject=""):
         time = datetime.datetime.now()
+        if msg == "":
+            msg = self.msg
+        if subject == "":
+            subject = "Notification"
 
-        # TODO: Update this to use self._config, and update it in general.
-        sender_email = "stopspot.noreply@gmail.com" # this needs sto be set up externally
-        receiver_email = ""
+        msg = "".join(["Subject: [StopSpot Pipeline] ", subject, " at ", str(time), "\n\n", msg])
 
-        # Create a secure SSL context
-        context = ssl.create_default_context()
+        password = self._update_email_data()
+        self._print("TO: ", self.user_email)
+        self._print("FROM: ", self.pipeline_email)
+        self._print("MSG:\n", msg)
 
         # TODO: catch smtplib exceptions
-        with smtplib.SMTP_SSL("smtp.gmail.com", self._port, context=context) as server:
-            server.login(sender_email, password)
-            server.sendmail(sender_email, receiver_email, msg)
+        #   smtplib.SMTPAuthenticationError
+
+        context = ssl.create_default_context() # Create a secure SSL context
+        server = smtplib.SMTP_SSL("smtp.gmail.com", self._port, context=context)
+        server.login(self.pipeline_email, password)
+        server.sendmail(self.pipeline_email, self.user_email, msg)
 
     # This method will update self.sender_email and
     # self.pipeline_email, and it will password, as well as prompting for
     # unavailable data.
+    # TODO: add this comment to the doc
+    # TODO: make the doc
     def _update_email_data(self):
         user_email = self._config.get_value("user_email")
         if user_email is None:
@@ -51,7 +60,7 @@ class _Notif(IOs):
 
         pipeline_email_passwd = self._config.get_value("pipeline_email_passwd")
         if pipeline_email_passwd is None:
-            pipeline_email_passwd = self._prompt("Please enter this pipeline's email: ")
+            pipeline_email_passwd = self._prompt("Please enter this pipeline's email password: ")
 
         return pipeline_email_passwd
 
