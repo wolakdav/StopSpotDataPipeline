438d0083458dd9730e8d214ae0ab2a5d86621f88
Sawyer Watts <sawyer.watts@gmail.com>
2020-04-18 11:54:42 -0700

    Finish _Notif._update_email_data() .

--- src/notif/notif.py
+++ src/notif/notif.py
@@ -6,8 +6,16 @@ from src.config import config
 class _Notif(IOs):
 
     def __init__(self, config):
+        super().__init__(True)
         self.msg = "A critial error has occured."
+        self._port = 465  # For SSL
         self._config = config
+
+    def print(self, string, obj=None, force=False):
+        raise AttributeError("AttributeError: " + self.__class__.__name__ + " has no attribute 'print'")
+
+    def prompt(self, prompt="", hide_input=False):
+        raise AttributeError("AttributeError: " + self.__class__.__name__ + " has no attribute 'prompt'")
 
     #######################################################
 
@@ -16,24 +24,36 @@ class _Notif(IOs):
         time = datetime.datetime.now()
 
         # TODO: Update this to use self._config, and update it in general.
-        port = 465  # For SSL
         sender_email = "stopspot.noreply@gmail.com" # this needs sto be set up externally
         receiver_email = ""
 
         # Create a secure SSL context
         context = ssl.create_default_context()
 
-        with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        # TODO: catch smtplib exceptions
+        with smtplib.SMTP_SSL("smtp.gmail.com", self._port, context=context) as server:
             server.login(sender_email, password)
             server.sendmail(sender_email, receiver_email, msg)
 
-    # TODO: this method will update self.port, self.sender_email, and
-    # self.receiver_email, and it will return None on failure and the password
-    # on success.
+    # This method will update self.sender_email and
+    # self.pipeline_email, and it will password, as well as prompting for
+    # unavailable data.
     def _update_email_data(self):
-        # TODO: this method needs to make sure that the data is actually in
-        # config, and if it isn't prompt via IOs for it.
-        pass
+        user_email = self._config.get_value("user_email")
+        if user_email is None:
+            user_email = self._prompt("Please enter the destination email: ")
+        self.user_email = user_email
+
+        pipeline_email = self._config.get_value("pipeline_email")
+        if pipeline_email is None:
+            pipeline_email = self._prompt("Please enter this pipeline's email: ")
+        self.pipeline_email = pipeline_email
+
+        pipeline_email_passwd = self._config.get_value("pipeline_email_passwd")
+        if pipeline_email_passwd is None:
+            pipeline_email_passwd = self._prompt("Please enter this pipeline's email: ")
+
+        return pipeline_email_passwd
 
     #######################################################
 
