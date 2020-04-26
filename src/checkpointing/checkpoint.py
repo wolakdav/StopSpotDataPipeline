af9b94b56aa695a4cccb446cd4bd58638d61f373
Erik-Hanson <ErikHanson135@gmail.com>
2020-04-25 20:25:59 -0700

    Modifying write_to_file() and read_from_file() implementation

--- src/checkpointing/checkpoint.py
+++ src/checkpointing/checkpoint.py
@@ -7,17 +7,23 @@ class Checkpoint:
 
     # This function will write to the checkpoint file
     # if there is an existing checkpoint it will overwrite that checkpoint
-    def write_to_file(self, date_to_checkpoint):
+    # date_to_checkpoint : Will be the date of the last row processed, this should be a string from a row entry
+    # end_date: Will be the ending date for the range of entries being queried, this should be a string from a row entry
+    def write_to_file(self, date_to_checkpoint, end_date):
         f = open(self._file, "w")  # Will create the file if it does not exists, if it does it overwrites
-        f.write(date_to_checkpoint.strftime("%Y-%m-%d"))
+        f.write(date_to_checkpoint + "," + end_date)
         f.close()
         return None
 
     # This function will read from the checkpoint file, it will get the last date that was checkpointed
     def read_from_file(self):
-        f = open(self._file, "r")
-        checkpoint_date = datetime.strptime(f.read(), "%Y-%m-%d")
-        return checkpoint_date  # returns a datetime object
+        with open(self._file, "r") as fstream:
+            for line in fstream:
+                dates = line.split(",")
+
+        # date_tuple is a tuple of datetime objects, this is to match the implementation within process_data() in main.py
+        date_tuple = (datetime.strptime(dates[0], '%Y-%m-%d'), datetime.strptime(dates[1], '%Y-%m-%d'))
+        return date_tuple
 
     # This function will rerun to enter all the data from the checkpoint date to the current date
     def rerun_from_checkpoint(self):
