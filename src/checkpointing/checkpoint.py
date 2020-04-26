242ec7e10bb665b1336c8db21dae9c8e672acbbf
Erik-Hanson <ErikHanson135@gmail.com>
2020-04-26 16:10:15 -0700

    Removing return Nones, changing filepath to use assets directory, and changing file opens to be more consistent with each other

--- src/checkpointing/checkpoint.py
+++ src/checkpointing/checkpoint.py
@@ -3,17 +3,15 @@ from datetime import datetime
 
 class Checkpoint:
     def __init__(self):
-        self._file = "checkpoint.txt"
+        self._file = "./assets/checkpoint.txt"
 
     # This function will write to the checkpoint file
     # if there is an existing checkpoint it will overwrite that checkpoint
     # date_to_checkpoint : Will be the date of the last row processed, this should be a string from a row entry
     # end_date: Will be the ending date for the range of entries being queried, this should be a string from a row entry
     def write_to_file(self, date_to_checkpoint, end_date):
-        f = open(self._file, "w")  # Will create the file if it does not exists, if it does it overwrites
-        f.write(date_to_checkpoint + "," + end_date)
-        f.close()
-        return None
+        with open(self._file, "w") as fstream:
+            fstream.write(date_to_checkpoint + "," + end_date)
 
     # This function will read from the checkpoint file, it will get the last date that was checkpointed
     def read_from_file(self):
@@ -29,4 +27,3 @@ class Checkpoint:
     def rerun_from_checkpoint(self):
         checkpoint_date = self.read_from_file()  # Date to run from
         # Need to figure out what function we need to call to rerun the stuffz
-        return None
