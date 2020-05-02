6b2501a2d55e96767f90e50947bb2b97772b44a0
Sawyer Watts <sawyer.watts@gmail.com>
2020-05-01 18:56:43 -0700

    Fill stub Processed_days.add_day() .

--- src/tables/processed_days.py
+++ src/tables/processed_days.py
@@ -1,4 +1,7 @@
 from .table import Table
+from sqlalchemy.engine.base import Engine
+from sqlalchemy.exc import SQLAlchemyError
+import datetime
 
 class Processed_Days(Table):
 
@@ -14,8 +17,34 @@ class Processed_Days(Table):
                 day DATE PRIMARY KEY
             );"""])
 
+    # This method will take string `day` in format YYYY/MM/DD and insert it to
+    # table processed_days.
     def add_day(self, day):
-        pass
+        if not isinstance(self._engine, Engine):
+            self._print("ERROR: invalid engine.")
+            return False
+
+        date = None
+        try:
+            date = datetime.datetime.strptime(day, "%Y-%m-%d")
+        except ValueError:
+            self._print("Error: The input date is malformed; please use the YYYY-MM-DD format.")
+            return False
+
+        sql = "".join(["INSERT INTO ", self._schema, ".", self._table_name,
+                       " (", self._index_col, ") VALUES ('",
+                        str(date.year), "/", str(date.month), "/", str(date.day),
+                        "');"])
+        self._print(sql)
+        try:
+            self._print("Connecting to DB.")
+            conn = self._engine.connect()
+            conn.execute(sql)
+        except SQLAlchemyError as error:
+            print("SQLAlchemyError: ", error)
+            return False
+        self._print("Done")
+        return True
 
     def delete_day(self, day):
         pass
