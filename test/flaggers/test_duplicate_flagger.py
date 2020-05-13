64bd6aa0a0889d03125ea02a857b178cbf233c7b
Sawyer Watts <sawyer.watts@gmail.com>
2020-05-13 07:34:17 -0700

    Adjust test_duplicate_flagger's indentation.

--- test/flaggers/test_duplicate_flagger.py
+++ test/flaggers/test_duplicate_flagger.py
@@ -4,41 +4,38 @@ import pandas
 import numpy as np
 
 class DataRow():
-  def __init__(self):
-    # NOTE: special values like nan and None showed up in sample data and
-    # caused a problem. numpy == cannot compare None for some reason, and had
-    # a tough time comparing NaN (i.e. required special solution).
-    self.row_id = "good"
-    self.service_date = "good"
-    self.vehicle_number = "good"
-    self.leave_time = "good"
-    self.train = "good"
-    self.route_number = "good"
-    self.direction = "good"
-    self.service_key = "good"
-    self.trip_number = "good"
-    self.stop_time = np.nan
-    self.arrive_time = "good"
-    self.dwell = "good"
-    self.location_id = "good"
-    self.door = "good"
-    self.ons = "good"
-    self.offs = "good"
-    self.estimated_load = "good"
-    self.lift = "good"
-    self.maximum_speed = None
-    self.train_mileage = "good"
-    self.pattern_distance = "good"
-    self.location_distance = "good"
-    self.x_coordinate = "good"
-    self.y_coordinate = "good"
-    self.data_source = "good"
-    self.schedule_status = "good"
-    self.trip_id = "good"
+    def __init__(self):
+        self.row_id = "good"
+        self.service_date = "good"
+        self.vehicle_number = "good"
+        self.leave_time = "good"
+        self.train = "good"
+        self.route_number = "good"
+        self.direction = "good"
+        self.service_key = "good"
+        self.trip_number = "good"
+        self.stop_time = np.nan
+        self.arrive_time = "good"
+        self.dwell = "good"
+        self.location_id = "good"
+        self.door = "good"
+        self.ons = "good"
+        self.offs = "good"
+        self.estimated_load = "good"
+        self.lift = "good"
+        self.maximum_speed = None
+        self.train_mileage = "good"
+        self.pattern_distance = "good"
+        self.location_distance = "good"
+        self.x_coordinate = "good"
+        self.y_coordinate = "good"
+        self.data_source = "good"
+        self.schedule_status = "good"
+        self.trip_id = "good"
 
 @pytest.fixture
 def duplicate_flagger():
-  return [f for f in flaggers if f.name == 'Duplicate'][0]
+    return [f for f in flaggers if f.name == 'Duplicate'][0]
 
 #Create list with just 1 item
 @pytest.fixture
